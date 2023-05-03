27a34e85e1d7803e5fa2af9122c7ba2bf6ac0045
john33mcd <jmcd-34@hotmail.com>
2023-05-03 19:07:48 +0000

    added view details

--- blog/views.py
+++ blog/views.py
@@ -2,20 +2,22 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
 
+
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 5
- 
+
+
 class PostDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filer(approved=True).order_by('created_on')
+        comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
-        if post.likes.filter(id=self.request.use.id).exists():
+        if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
         return render(
@@ -25,5 +27,5 @@ class PostDetail(View):
                 "post": post,
                 "comments": comments,
                 "liked": liked
-            }
+            },
         )
